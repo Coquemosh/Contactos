fc934514bab65288db42bebbd64ee55a2d29b68f
Jorge Lira <coque_16@hotmail.com>
2013-08-10 19:48:10 -0500

    ASDF

--- js/actions.js
+++ js/actions.js
@@ -1,51 +1,5 @@
 // Actions
-$(function(){
-    
-document.addEventListener("deviceready",function(){
-    $('#cBtn').tap(function(){
-        var nom = $('#cName').val();
-        var ema = $('#cMail').val();
-        var tel = $('#cTel').val();
-        
-        if(nom!='' && ema!='' && tel!=''){
-            //alert(nom+' '+ema+' '+tel);
-            crearContacto(nom, ema, tel);
-        }
-    });
-    
-    $('#btnListar').tap(function(){
-        listarContactos();
-    });
-},false);
-});
 
-function listarContactos(){
-    function onSuccess(contacts) {
-        $'#lista').html(''); // lo vaciamos aunque no tenga nada.
-        for(i=0;i<contacts.length;i++){
-            $('<li class="forward"><a href="tel:'+contacts[i].phoneNumbers[0].value+'">'+contacts[i].name.formatted+'</a></li>').appendTo('#lista');
-        }
-    };
-    
-    function onError(contactError) {
-        alert('onError!');
-    };
-    
-    // find all contacts with 'Bob' in any name field
-    var options      = new ContactFindOptions();
-    //options.filter   = "Bob";
-    options.filter   = "";
-    options.multiple = true;
-    //var fields       = ["displayName", "name"]; // para listar solamente el displayName y name
-    var fields       = ["*"];
-    navigator.contacts.find(fields, onSuccess, onError, options);
-}
-
-function crearContacto(nom,ema,tel){
-    alert('WTF');
-    // Creamos Contacto
-    var myContact = navigator.contacts.create();
-    
     
     /*
     http://docs.phonegap.com/en/3.0.0/cordova_contacts_contacts.md.html#ContactName
@@ -80,29 +34,63 @@ function crearContacto(nom,ema,tel){
     urls: An array of web pages associated with the contact. (ContactField[])
 
     */
-    // Asignamos un Nombre para mostrar
-    myContact.displayName = nom; 
-    myContact.nickname = nom;
+$(function(){
+    document.addEventListener("deviceready",function(){
+        $('#cBtn').tap(function(){
+            var nom = $('#cName').val();
+            var ema = $('#cMail').val();
+            var tel = $('#cTel').val();
+            
+            if(nom!='' && ema!='' && tel!='')
+                crearContacto(nom,ema,tel);
+        });
+        $('#btnListar').tap(function(){
+            listarContactos();
+        });
+    },false);
+});
+
+function listarContactos(){
+    function onSuccess(contacts){
+        $('#lista').html('');
+        for(i=0;i<contacts.length;i++){
+            $('<li class="forward"><a href="tel:'+contacts[i].phoneNumbers[0].value+'">'+contacts[i].name.formatted+'</a></li>').appendTo('#lista');
+        }
+    };
     
-    // Asignar nombre del Contacto
+    function onError(contactError) {
+        alert('onError!');
+    };
+    
+    var options      = new ContactFindOptions();
+    options.filter   = "";
+    options.multiple = true;
+    var fields       = ["*"];
+    navigator.contacts.find(fields, onSuccess, onError, options);
+}
+
+function crearContacto(nom,ema,tel){
+    //Creamos Contacto
+    var myContact = navigator.contacts.create();
+    //Asignarle un nombre para mostrar
+    myContact.displayname = nom;
+    myContact.nickname = nom;
+    //Asignar nombre del Contacto
     var nombre = new ContactName();
     nombre.givenName = nom;
     myContact.name = nombre;
-    
-    // Asignamos el email
+    //Asignar email del Contacto
     var email = [];
     email[0] = new ContactField("home",ema,true);
     email[1] = new ContactField("work","ejemplo@algo.com",false);
     myContact.emails = email;
-    
-    // Asignamos el teléfono
+    //Asignar teléfono del Contacto
     var telefono = [];
     telefono[0] = new ContactField("home",tel,false);
-    telefono[1] = new ContactField("work","222-2222",true); 
+    telefono[1] = new ContactField("work","222-2222",true);
     myContact.phoneNumbers = telefono;
-    
     //Guardar Contacto
-     myContact.save(function(){
+    myContact.save(function(){
         $('#cName').val('');
         $('#cMail').val('');
         $('#cTel').val('');
